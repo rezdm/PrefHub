fbc731471f1f31c454710ae03a3dc5cce3b552b5
Dmitry Reznitsky <reznitsky@gmail.com>
2025-10-06 11:21:55 +0200

    Reshuffle build structure; remove older cli client; move scripts

---
+++ 65-start-client-scenarios.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+./prefhub-scenarios-client/scenario_runner.py test0
+
--- start-client.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-cd prefhub-client
-mvn exec:java -Dexec.mainClass="com.prefhub.client.ClientMain" -Dexec.args="http://localhost:8090"
