6a45c81c3d0b5877e126b722a70f29c6b6c417b5
Dmitry Reznitsky <reznitsky@gmail.com>
2025-10-13 19:31:34 +0200

    Slightly update front-end startup script

--- 30-start-frontend.sh
+++ 30-start-frontend.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Start PrefHub React frontend
 
+# Load nvm and use Node.js 22
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use 22 > /dev/null 2>&1
+
 cd "$(dirname "$0")/prefhub-frontend"
 
 echo "Starting PrefHub frontend on http://localhost:3000"
